19d6916afe533d36091f9630d293e8652469087c
TONI <antoniomiguel12345677@gmail.com>
2023-12-08 22:44:07 +0000

    Revert "Revert "Add functions to use""
    This reverts commit 974ee624a7acf33c221c60e6a31faa7763d6df0f.

--- Libs/Admin.h
+++ Libs/Admin.h
@@ -1,7 +1,10 @@
 #ifndef COMPANY_MANAGEMENT_ADMIN_H
 #define COMPANY_MANAGEMENT_ADMIN_H
-
-
+#include "structs.h"
+void createCompany(Companies *companies);
+int findCompanyIndexByNif(const Companies *companies, int nif);
+void deleteCompany(Companies *companies);
+void modifyCompany(Companies *companies);
 
 
 #endif
