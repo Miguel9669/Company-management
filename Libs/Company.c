64eb2cd0e7a180d7a313d5d00864cc7590e9bac1
Miguel <josemiguelrocha2000013@gmail.com>
2024-01-08 17:56:21 +0000

    admin documentation

--- Libs/Company.c
+++ Libs/Company.c
@@ -1,13 +1,17 @@
 #include "Company.h"
-#include <stdio.h>
 #include "structs.h"
-#include "Geral.h"
-#include "Admin.h"
-#include <string.h>
-#include "constVariables.h"
 #include "Menus.h"
-#include "user.h"
 
+/**
+ * @brief Handles actions for a specific company.
+ *
+ * This function enters a loop to handle various actions related to a specific company based on user input,
+ * and it continues until the user chooses to go back.
+ *
+ * @param companies Pointer to the Companies structure.
+ * @param activities Pointer to the Activities structure.
+ * @param informations Informations structure.
+ */
 void handleCompany(Companies *companies, Activities *activities, Informations informations){
     bool back;
     do {
@@ -16,7 +20,16 @@ void handleCompany(Companies *companies, Activities *activities, Informations in
     } while (back != true);
 }
 
-
+/**
+ * @brief Handles actions for company reports.
+ *
+ * This function enters a loop to handle various actions related to company reports based on user input,
+ * and it continues until the user chooses to exit the menu.
+ *
+ * @param company Pointer to the Company structure.
+ * @param informations Informations structure.
+ * @param index Index of the company.
+ */
 void handleMenuReports(Company *company, Informations informations, int index) {
     int option;
     do {
