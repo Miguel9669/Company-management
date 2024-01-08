0ce3928f6a730553685b5537928e0c930eb74bc4
Miguel <josemiguelrocha2000013@gmail.com>
2024-01-08 17:53:21 +0000

    admin documentation

--- Libs/Admin.c
+++ Libs/Admin.c
@@ -7,8 +7,15 @@
 #include "constVariables.h"
 #include "Menus.h"
 
-/* This function provides an interface for performing administrative tasks on companies and activities.
- * It uses a menu system to allow the admin to navigate through various options and perform actions
+/**
+ * @brief Handles administrative actions.
+ *
+ * This function enters a loop to handle various administrative actions based on user input,
+ * and it continues until the user chooses to go back.
+ *
+ * @param companies Pointer to the Companies structure.
+ * @param activities Pointer to the Activities structure.
+ * @param informations Pointer to the Informations structure.
  */
 void handleAdmin(Companies *companies, Activities *activities, Informations *informations){
     bool back;
@@ -18,20 +25,17 @@ void handleAdmin(Companies *companies, Activities *activities, Informations *inf
     } while (back != true);
 }
 
-/* @brief Creates a new company and adds it to the Companies structure.
+/**
+ * @brief Creates a new company and adds it to the Companies structure.
  *
  * This function is used to create a new company and adds it to the Companies structure.
  * It collects information such as the company's name, nif, address, city, postal code, category, initializes the new company's
  * rates and comments, initialize the company active in true and update the number from file and the struct company.
  *
- * @param[in,out] companies A pointer to the Companies structure to which the new company will be added.
- * @param[in,out] activities A pointer to the Activities structure containing available activities for the company.
- * @param[in,out] informations A pointer to the Informations structure containing additional information.
- *
- * @return void
- *
+ * @param companies A pointer to the Companies structure to which the new company will be added.
+ * @param activities A pointer to the Activities structure containing available activities for the company.
+ * @param informations A pointer to the Informations structure containing additional information.
  */
-
 void createCompany(Companies *companies, Activities *activities, Informations *informations) {
     reallocInStruct(informations->numberInformation, informations->maxInformation, NULL, NULL, informations,INFORMATIONS);
     reallocInStruct(companies->numberCompanies, companies->maxCompanies, companies, NULL, NULL, COMPANIES);
@@ -54,6 +58,15 @@ void createCompany(Companies *companies, Activities *activities, Informations *i
     updateStructCompany(FILE_WITH_COMPANIES, sizeof(Company) * (companies->numberCompanies - 1), &companies->company[numberCompanies], sizeof(Company));
 }
 
+/**
+ * @brief Initializes information.
+ *
+ * This function increments the total number of information and initialize several variables to zero in this case
+ * searchByNameCounter, searchByCategoryCounter, searchByActivityCounter and searchCounter.
+ *
+ * @param informations Pointer to the Information structure.
+ * @param index of the information to be initialized.
+ */
 void iniciateInformation(Informations *informations, int index) {
     informations->numberInformation++;
     Information *information = &informations->information[index];
@@ -62,6 +75,16 @@ void iniciateInformation(Informations *informations, int index) {
     information->searchByActivityCounter = 0;
     information->searchCounter = 0;
 }
+
+/**
+ * @brief Initializes comments and rates for a Company.
+ *
+ * This function aims to initialize several variables related to company comments and rates.
+ * It sets the sum of rates to 0, the number of comments and rates to 0, and allocates memory
+ * for storing comments with an initial capacity. It also initializes individual comment properties.
+ *
+ * @param company Pointer to the Company structure.
+ */
 void iniciateCommentsAndRates(Company *company) {
     company->sumRates = 0;
     company->numberComments = company->numberRates = 0;
@@ -75,6 +98,18 @@ void iniciateCommentsAndRates(Company *company) {
     }
 }
 
+/**
+ * @brief Deletes a company.
+ *
+ * This function removes a company at the specified index from the
+ *
+ * This function removes a company at the specified index from the list of companies.
+ * If the company has comments, it cannot be deleted but can be set as an inactive company.
+ * The function updates the Companies structure and writes the changes to a file.
+ *
+ * @param companies Pointer to the Companies structure.
+ * @param index Index of the company to be deleted.
+ */
 void deleteCompany(Companies *companies, int index) {
     if (companies->company[index].numberComments > 0) {
         puts("You can not delete this company! But u can set as an inactive company.");
@@ -99,6 +134,15 @@ void deleteCompany(Companies *companies, int index) {
     updateNumberFromFile(companies->numberCompanies, FILE_NUMBER_COMPANIES_NAME);
 }
 
+/**
+ * @brief Creates a new activity.
+ *
+ * This function dynamically reallocates memory to accommodate a new activity in the list.
+ * It then prompts the user to enter details for the new activity, sets it as active,
+ * increments the total number of activities, and updates the external files.
+ *
+ * @param activities Pointer to the Activities structure.
+ */
 void creatActivity(Activities *activities){
     reallocInStruct(activities->numberActivities, activities->maxActivities, NULL, activities, NULL, ACTIVITIES);
     createNameActivity(activities);
@@ -108,6 +152,16 @@ void creatActivity(Activities *activities){
     updateStructActivities(FILE_WITH_ACTIVITIES, sizeof(Activity) * (activities->numberActivities - 1), &activities->activities[activities->numberActivities - 1], sizeof(Activity));
 }
 
+/**
+ * @brief Deactivates an activity and updates related companies accordingly.
+ *
+ * This function deactivates the specified activity and updates the external files
+ * with the changes. It also sets the related companies as inactive.
+ *
+ * @param activity Pointer to the Activity structure to be deactivated.
+ * @param companies Pointer to the Companies structure.
+ * @param index Index of the activity to be deactivated.
+ */
 void inactiveActivity(Activity *activity, Companies *companies, int index) {
     activity->Active = false;
     updateStructActivities(FILE_WITH_ACTIVITIES, sizeof(Activity) * index, activity, sizeof(Activity));
@@ -119,6 +173,16 @@ void inactiveActivity(Activity *activity, Companies *companies, int index) {
     }
 }
 
+/**
+ * @brief Activates an activity and updates related companies accordingly.
+ *
+ * This function activates the specified activity and updates the external files
+ * with the changes. It also sets the related companies as active.
+ *
+ * @param activity Pointer to the Activity structure to be activated.
+ * @param companies Pointer to the Companies structure.
+ * @param index Index of the activity to be activated.
+ */
 void activeActivity(Activity *activity, Companies *companies, int index) {
     activity->Active = true;
     updateStructActivities(FILE_WITH_ACTIVITIES, sizeof(Activity) * index, activity, sizeof(Activity));
@@ -130,6 +194,15 @@ void activeActivity(Activity *activity, Companies *companies, int index) {
     }
 }
 
+/**
+ * @brief Handles administrative actions related to activities.
+ *
+ * This function displays a menu of administrative actions related to activities,
+ * and based on user input, it either performs the selected action or creates a new activity.
+ *
+ * @param activities Pointer to the Activities structure.
+ * @param companies Pointer to the Companies structure.
+ */
 void handleAdminActivity(Activities *activities, Companies *companies) {
     int optionActivity;
     do {
@@ -143,6 +216,15 @@ void handleAdminActivity(Activities *activities, Companies *companies) {
     } while (optionActivity != -1);
 }
 
+/**
+ * @brief Handles administrative reports.
+ *
+ * This function enters a loop to handle administrative reports based on user input,
+ * and it continues until the user chooses to go back.
+ *
+ * @param companies Companies structure.
+ * @param informations Informations structure.
+ */
 void handleAdminReports(Companies companies, Informations informations) {
     bool back;
     do {
@@ -150,6 +232,16 @@ void handleAdminReports(Companies companies, Informations informations) {
         menuAdminReports(&back, companies, informations, 2, 2);
     } while (!back);
 }
+
+/**
+ * @brief Deletes an activity.
+ *
+ * This function removes an activity at the specified index from the list of activities.
+ * It shifts the remaining activities to fill the gap and updates the Activities structure accordingly.
+ *
+ * @param activities Pointer to the Activities structure.
+ * @param index Index of the activity to be deleted.
+ */
 void deleteActivity(Activities *activities, int index) {
     for (int i = index; i < activities->numberActivities - 1; i++) {
         activities->activities[i] = activities->activities[i + 1];
@@ -159,6 +251,21 @@ void deleteActivity(Activities *activities, int index) {
     updateNumberFromFile(activities->numberActivities, FILE_NUMBER_ACTIVITIES_NAME);
 }
 
+/**
+ * @brief Modifies a company based on user input and menu options.
+ *
+ * This function allows the user to input a company's NIF, finds the corresponding company,
+ * and then enters a menu loop for modifying various aspects of the company based on user choices.
+ * The modification options are presented through the menu provided by the `menuModify` function.
+ *
+ * @param companies Pointer to the Companies structure.
+ * @param activities Pointer to the Activities structure.
+ * @param userType Type of user (user, admin or company).
+ * @param txt Additional text for user interaction.
+ * @param min Minimum menu option.
+ * @param max Maximum menu option.
+ * @param informations Informations structure.
+ */
 void modifyCompany(Companies *companies, Activities *activities, Type userType, char *txt, int min, int max, Informations informations) {
     int nif, index, opcao;
 
@@ -178,6 +285,15 @@ void modifyCompany(Companies *companies, Activities *activities, Type userType, 
     }
 }
 
+/**
+ * @brief Delete comment for a specific Company.
+ *
+ *This function removes a comment at the specified index from the Company list of comments.
+ *After that the function shifts the remaining comments to fill the gap and updates the Company structure accordingly.
+ *
+ * @param company Pointer to the Company structure.
+ * @param index Index of the comment to be deleted.
+ */
 void deleteComment(Company *company, int index) {
     for (int i = index; i < company->numberComments - 1; i++) {
         company->comments[i] = company->comments[i + 1];
@@ -186,6 +302,16 @@ void deleteComment(Company *company, int index) {
     updateStructCompany(FILE_WITH_COMPANIES, sizeof(Company) * (index + 1), company, sizeof(Company));
 }
 
+/**
+ * @brief Hides or display a comment for a specific Company.
+ *
+ * This function toggles the visibility state of a comment at the specified index for a Company.
+ * If the comment is currently visible, it hides it, otherwise, it display it.
+ * The function updates the Company structure and writes the changes to a file.
+ *
+ * @param company Pointer to the Company structure.
+ * @param index Index of the comment to be hidden or unhidden.
+ */
 void hideComments(Company *company, int index) {
     for (int i = 0; i < company->numberComments; i++) {
         if (i == index) {
--- Libs/Admin.h
+++ Libs/Admin.h
@@ -1,9 +1,7 @@
 #ifndef COMPANY_MANAGEMENT_ADMIN_H
 #define COMPANY_MANAGEMENT_ADMIN_H
-
-
-
 #include "structs.h"
+
 void createCompany(Companies *companies, Activities *activities, Informations *informations);
 void deleteCompany(Companies *companies, int index);
 void modifyCompany(Companies *companies, Activities *activities, Type userType, char *txt, int min, int max, Informations informations);
@@ -11,12 +9,13 @@ void handleAdmin(Companies *companies, Activities *activities, Informations *inf
 void iniciateCommentsAndRates(Company *company);
 void iniciateInformation(Informations *informations, int index);
 void creatActivity(Activities *activities);
-void inactiveActivity(Activity *activity, Companies *companies, int index);
 void deleteActivity(Activities *activities, int index);
+void inactiveActivity(Activity *activity, Companies *companies, int index);
 void activeActivity(Activity *activity, Companies *companies, int index);
 void handleAdminActivity(Activities *activities, Companies *companies);
 void deleteComment(Company *company, int index);
-void handleAdminReports(Companies companies, Informations companiesExtraInformation);
 void hideComments(Company *company, int index);
+void handleAdminReports(Companies companies, Informations companiesExtraInformation);
+
 
 #endif
